b17d399b052985934d6a8b81030ded646e704bf7
Michael Hearn <git@mikehearn.net>
2023-08-22 16:19:30 -0400

    Pin lxml to 4.9.2

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(
         'Salesforce-FuelSDK @ git+https://github.com/nypublicradio/FuelSDK-Python.git',
         'Werkzeug==1.0.1', # for compatitility with Flask 1.x
         'boto3~=1.21',
+        'lxml==4.9.2',
         'flask==1.1.4',
         'markupsafe==2.0.1',
         'python-dotenv',
